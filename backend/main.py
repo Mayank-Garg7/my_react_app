22e8a6e59027fe837e0026492791e2a468940992
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-06-05 12:49:22 +0530

    setup the middleware and data and ready to communicate

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,26 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 app.app_middleware(
-    
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173/"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-
+tasks = [
+    {
+        "id": 1,
+        "text": "did You do stretching today",
+        "status": "pending",
+        "priority": "moderate"
+    },
+    {
+        "id": 2,
+        "text": "did you learn something new today",
+        "status": "pending",
+        "priority": "high"
+    }
+]
 @app.get("/")
 def greet():
     return {"message": "hello mayank welcome in fastApi Era"}

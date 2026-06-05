1cf01ff0a8069329caf246f840cfae33133310a4
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2026-06-05 12:45:46 +0530

    add middleware to make the communication smooth with frontend and backend

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.app_middleware(
+    
+)
+
 @app.get("/")
 def greet():
     return {"message": "hello mayank welcome in fastApi Era"}
